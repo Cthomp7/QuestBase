8796aa3a95f30951aa84a0147f210501c5a493da
Cthomp7 <cthomp7@saic.edu>
2025-03-17 17:30:21 -0500

    added session highlights & upcoming session sections to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import sessions from "./data/sessions.json";
+import upcomingSessions from "./data/upcoming.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Session = ({
+    title,
+    date,
+    description,
+  }: {
+    title: string;
+    date: string;
+    description: string;
+  }) => {
+    return (
+      <div className="session-item">
+        <p className="session-title">{title}</p>
+        <p className="session-date">{date}</p>
+        <div className="session-hr"></div>
+        <p className="session-description">{description}</p>
+      </div>
+    );
+  };
+
+  const UpcomingSession = ({ date, time }: { date: string; time: string }) => {
+    return (
+      <div className="upcoming-item">
+        <p className="upcoming-date">{date}</p>
+        <p className="upcoming-time">{time}</p>
+      </div>
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <h1>:{")"} Codexia</h1>
+        <div className="navigation">
+          <a href="#">session recaps</a>
+        </div>
+      </header>
+      <div className="sections">
+        <section className="session-highlights">
+          <h2>Session Highlights</h2>
+          <div className="sessions-container">
+            {sessions.slice(0, 3).map((session) => (
+              <Session key={session.title} {...session} />
+            ))}
+            <button>search all</button>
+          </div>
+        </section>
+        <section style={{ display: "flex", alignItems: "center" }}>
+          &#9888; Coming soon &#9888;
+        </section>
+        <section>
+          <h2>Upcoming Sessions</h2>
+          <div className="upcoming-container">
+            {upcomingSessions.map((session) => (
+              <UpcomingSession {...session} />
+            ))}
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
