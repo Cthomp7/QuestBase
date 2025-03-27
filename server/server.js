97afb073bd12efc1c73ce2f1984d0e254b287208
Cthomp7 <cthomp7@saic.edu>
2025-03-26 20:19:37 -0500

    fixed fallback path

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(appDirectory));
 
 // Fallback to index.html for client-side routing
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
 // Read directory recursively (same as before)
