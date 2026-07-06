c304e6e27b68de94de0a04361081b9e7209b53c8
Cthomp7 <cthomp7@saic.edu>
2026-07-05 21:10:42 -0500

    cleaned up Header component + adjusted styles

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,25 +1,8 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import icon from "./../../assets/favicon-32x32.png";
 import styles from "./Header.module.css";
-import AuthModal from "./AuthModal";
-import { useAuth } from "@/context/AuthContext";
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const Header = () => {
-  const { logout } = useAuth();
-  const [showLogin, setShowLogin] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(() => {
-    return localStorage.getItem("loggedIn") === "true";
-  });
-
-  const handleLogout = () => {
-    logout();
-    setLoggedIn(false);
-    localStorage.removeItem("loggedIn");
-  };
-
   return (
     <header>
       <Link className={styles.title} to={"/"}>
@@ -30,20 +13,10 @@ const Header = () => {
         <Link to="/">Base</Link>
         <Link to="/codex">Codex</Link>
         <Link to="/npcs">NPCs</Link>
-        <p>|</p>
+      </div>
+      <div className={styles.navigation}>
         <Link to="/login">Login</Link>
       </div>
-      {showLogin && !loggedIn && (
-        <AuthModal
-          show={showLogin}
-          onClose={() => setShowLogin(false)}
-          onLoginSuccess={() => {
-            setLoggedIn(true);
-            localStorage.setItem("loggedIn", "true");
-          }}
-          apiBaseUrl={API_BASE_URL}
-        />
-      )}
     </header>
   );
 };
