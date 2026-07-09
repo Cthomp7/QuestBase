e5d0a5e3325ed9eaf8cff88cb5ae42627a0b13f6
Cthomp7 <cthomp7@saic.edu>
2026-07-08 19:39:12 -0500

    restructured App.tsx + added a public layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,30 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/page";
 import Codex from "./pages/codex/page";
 import "./App.css";
-import Header from "./components/Header/Header";
 import NPCs from "./pages/npcs/page";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Campaigns from "./pages/campaigns/Campaigns";
+import PublicLayout from "./layouts/PublicLayout";
 // import NotFound from "./pages/NotFound";
 
 const App: React.FC = () => {
   return (
     <Router>
-      {<Header />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/campaigns" element={<Campaigns />}/>
-        <Route path="/codex" element={<Codex />} />
-        <Route path="/codex/*" element={<Codex />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/npcs/" element={<NPCs />} />
-        <Route path="/npcs/*" element={<NPCs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+
+        <Route>
+          <Route path="/campaigns" element={<Campaigns />}/>
+          <Route path="/codex/*" element={<Codex />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/npcs/*" element={<NPCs />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/layouts/PublicLayout.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header/Header"
+
+
+const PublicLayout = () => {
+  return (
+    <Header></Header>
+  )
+}
+
+export default PublicLayout
