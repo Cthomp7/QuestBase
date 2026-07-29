66e9b99236e1452bf332244b5d0734ee418f4a84
Cthomp7 <cthomp7@saic.edu>
2026-07-29 14:47:57 -0500

    removed localhost from /auth/register api call

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -19,7 +19,7 @@ function Register () {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ displayName: name, email, password })
