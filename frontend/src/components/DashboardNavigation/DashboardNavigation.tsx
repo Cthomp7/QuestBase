68703bec7e8cf627c93528f6a341b4a4731db7b0
Cthomp7 <cthomp7@saic.edu>
2026-08-06 09:53:58 -0500

    added user navigation menu for dashboard navigation

--- frontend/src/components/DashboardNavigation/DashboardNavigation.tsx
+++ frontend/src/components/DashboardNavigation/DashboardNavigation.tsx
@@ -3,11 +3,17 @@ import styles from "./DashboardNavigation.module.css"
 import { useEffect, useState } from "react"
 import Dropdown, { DropdownOption } from "@/components/Dropdown/Dropdown"
 import { useCampaign } from "@/context/campaign/useCampaign"
+import ProfilePicture from "@/assets/imgs/profiles/Ribbert.png"
+import { useAuth } from "@/context/AuthContext"
+import MenuDropdown from "../MenuDropdown/MenuDropdown"
+import { ArrowLeft } from "lucide-react"
 
 const DashboardNavigation = () => {
   const navigate = useNavigate()
+  const { logout, user } = useAuth()
   const { campaigns, activeCampaignId, setActiveCampaignId } = useCampaign()
   const [campaignDropdownOptions, setCampaignDropdownOptions] = useState<DropdownOption[]>([])
+  const [accountMenuOpen, setAccountMenuOpen] = useState(false)
 
   const pages = [
     { name: "Quests", url: "/quests" }
@@ -31,33 +37,78 @@ const DashboardNavigation = () => {
     localStorage.setItem("activeCampaignId", id);
   }
 
+  const redirectToLogin = () => {
+    navigate("/login")
+  }
+
   return (
     <div className={styles.dashboard_navigation}>
-      <section>
-        <Link to="/"><h1>Quest<span>Base</span></h1></Link>
-      </section>
-      <section className={styles.navigation_section}>
-        <div className={styles.campaign_container}>
-          <p>Campaign:</p>
-          <button onClick={() => navigate("/campaigns")}>view all</button>
-        </div>
-        <Dropdown
-          options={campaignDropdownOptions}
-          value={activeCampaignId}
-          onChange={(c) => switchCampaign(c)}
-        ></Dropdown>
-      </section>
-      <section className={styles.navigation_pages}>
-        {pages.map((p) => (
-          <div
-            key={p.url}
-            className={styles.navigation_page}
-            onClick={() => navigate(p.url)}
-          >
-            <p>{p.name}</p>
+      <div>
+        <section className={styles.dashboard_header}>
+          <div className={styles.dashboard_header_qb}>
+            <ArrowLeft className={styles.qb_arrow}/>
+            <Link to="/"><h1>Quest<span>Base</span></h1></Link>
           </div>
-        ))}
-      </section>
+          <div className={styles.dashboard_user_navigation}>
+            <img 
+              src={ProfilePicture} 
+              alt="Profile Picture of Ribbert the Frog"
+              className={styles.profile_picture} 
+              onClick={() => setAccountMenuOpen(!accountMenuOpen)}
+            />
+            <div className={styles.tooltip}>Open user navigation menu</div>
+          </div>
+          <MenuDropdown
+            open={accountMenuOpen}
+            style={{ 
+              top: 0,
+              left: '100%', 
+              margin: "0 0 0 20px",
+              width: "100%"
+            }}
+            children={
+              <>
+                <div className={styles.account_info}>
+                  <img 
+                    src={ProfilePicture} 
+                    alt="Profile Picture of Ribbert the Frog"
+                    className={styles.profile_picture}
+                  />
+                  <div>
+                    <p className={styles.user_display_name}>{user?.displayName}</p>
+                    <p className={styles.user_email}>{user?.email}</p>
+                  </div>
+                </div>
+                <hr />
+                <Link to="/settings">Settings</Link>
+                <Link onClick={() => logout(redirectToLogin)} to="#">Logout</Link>
+              </>
+            }
+          ></MenuDropdown>
+        </section>
+        <section className={styles.navigation_section}>
+          <div className={styles.campaign_container}>
+            <p>Campaign:</p>
+            <button onClick={() => navigate("/campaigns")}>view all</button>
+          </div>
+          <Dropdown
+            options={campaignDropdownOptions}
+            value={activeCampaignId}
+            onChange={(c) => switchCampaign(c)}
+          ></Dropdown>
+        </section>
+        <section className={styles.navigation_pages}>
+          {pages.map((p) => (
+            <div
+              key={p.url}
+              className={styles.navigation_page}
+              onClick={() => navigate(p.url)}
+            >
+              <p>{p.name}</p>
+            </div>
+          ))}
+        </section>
+      </div>
     </div>
   )
 }
